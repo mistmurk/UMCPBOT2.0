23d6f442cc32e04cb4424e954ffca296a1129fde
mistmurk <lukev21@gmail.com>
2019-11-19 21:04:28 -0500

    help formatting

--- commands/help.js
+++ commands/help.js
@@ -7,26 +7,13 @@ module.exports = {
   guildOnly: true, // does this command only work in a guild?
   adminOnly: false, // is this command only for admins?
   async execute(client, db, message, args) {
-    if(args.length == 0) {
-      var helptext = "```"
-      client.commands.forEach(command => {
-        if(!command.adminOnly) {
-          helptext += command.name + ": \t" + command.description + "\n"
-        }
-      });
-      helptext += "```\n"
-      message.channel.send(helptext);
-    } else {
-      var command = client.commands.get(commandName)
-      || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
-      if(command) {
-        var helptext = "```\n";
-        helptext += command.name + '\n\n' + command.usage + '\n' + command.description;
-        helptext += "\n```";
-        message.channel.send(helptext);
-      } else {
-        message.channel.send('The requested command was not found.')
+    var helptext = "```"
+    client.commands.forEach(command => {
+      if(!command.adminOnly) {
+        helptext += command.name + ":\n\t" + command.description + "\n"
       }
-    }
+    });
+    helptext += "```\n"
+    message.channel.send(helptext);
   },
 }
