464bd51c125a5e8b0a6fd12091baeae4fe0fd5c9
mistmurk <lukev21@gmail.com>
2019-11-19 20:49:47 -0500

    fixed filtering logic

--- messages.js
+++ messages.js
@@ -6,9 +6,9 @@ module.exports = async (client, db, message) => {
   if (message.author.bot) return;
 
   // Ignore messages not in role-request and bot-commands
-  if(message.guild != '348919724635324419' ||
-     message.channel == '646521417805856768' ||
-     message.channel == '349781614877999104') {
+  if(message.guild == '348919724635324419' &&
+     !(message.channel == '646521417805856768' ||
+     message.channel == '349781614877999104')) {
        return;
      }
 
